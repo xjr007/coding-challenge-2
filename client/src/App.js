533da623e075b2885ed2e050026478be0886e755
abaaqielbehardien@gmail.com <abaaqielbehardien@gmail.com>
2020-09-10 18:05:17 +0200

    initialised scss | added basic styling | DEL App.css and RM import of old App.css in App.js and imported new style.css into App.js | added /assets/css & /assets/img | added /components & /components/layout | added Slider.js in /components | added Navigation.js in /components/layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Slider from './components/Slider';
+
+import './assets/css/style.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Clean App created...</h1>
+			<Slider />
 		</div>
 	);
 }
