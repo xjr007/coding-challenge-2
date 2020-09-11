a4a8f53410de54430d62ae5d823a11de515bd9fe
abaaqielbehardien@gmail.com <abaaqielbehardien@gmail.com>
2020-09-11 11:21:23 +0200

    created object in slide with image and descr for image | changed description to go inside slider to change with image | also made tooltip absolute to be outside of button | not enough time to change styling so that I am happy with it | works without test

--- client/src/layout/InformationBox.js
+++ client/src/layout/InformationBox.js
@@ -1,17 +1,10 @@
 import React from 'react';
 
-const InformationBox = () => {
+const InformationBox = props => {
+	const { description } = props;
 	return (
 		<div className='info'>
-			<p id='description'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-				accusantium, incidunt delectus illum suscipit laboriosam aspernatur
-				fuga quod. Pariatur repellendus magni quaerat facilis natus
-				molestias aperiam veritatis aspernatur, explicabo ex possimus,
-				doloribus voluptatum amet impedit quos iste cupiditate saepe
-				laudantium magnam placeat praesentium aliquid! Est illo ex minus
-				incidunt maxime!
-			</p>
+			<p id='description'>{description}</p>
 		</div>
 	);
 };
--- client/src/layout/Slider.js
+++ client/src/layout/Slider.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slide from '../components/Slide';
+import Slide1 from '../assets/img/Slide1.jpg';
+import Slide2 from '../assets/img/Slide2.jpg';
+import Slide3 from '../assets/img/Slide3.jpg';
+import Slide4 from '../assets/img/Slide4.jpg';
+import Slide5 from '../assets/img/Slide5.jpg';
 
 const Slider = () => {
+	const slides = [
+		{
+			imgURL: Slide1,
+			imgDescription:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam accusantium ipsum sint nobis libero minima possimus repudiandae consequuntur nisi officiis?',
+		},
+		{
+			imgURL: Slide2,
+			imgDescription:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus temporibus corporis facere, obcaecati modi.',
+		},
+		{
+			imgURL: Slide3,
+			imgDescription:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, blanditiis vero officiis autem voluptatum soluta nisi molestiae amet nihil corporis ratione. Voluptates aspernatur distinctio similique amet exercitationem est a quia?',
+		},
+		{
+			imgURL: Slide4,
+			imgDescription:
+				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, soluta.',
+		},
+		{
+			imgURL: Slide5,
+			imgDescription:
+				'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro rem, sed expedita tempora tenetur, ratione beatae neque illum, ipsam quasi dolore. Minus obcaecati veniam voluptatibus?',
+		},
+	];
 	return (
 		<div>
-			<Slide />
+			<Slide slides={slides} />
 		</div>
 	);
 };
